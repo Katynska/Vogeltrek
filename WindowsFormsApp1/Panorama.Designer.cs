eab8b3e7b6cb2ff6d4053d8dc8abb3848e3572ee
Grigory <povolyaev@yahoo.com>
2024-01-29 18:32:38 +0300

    Изучение, день 2

--- WindowsFormsApp1/Panorama.Designer.cs
+++ WindowsFormsApp1/Panorama.Designer.cs
@@ -54,9 +54,11 @@
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.trackBar1);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(771, 12);
+            this.groupBox1.Location = new System.Drawing.Point(1156, 18);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(314, 306);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(471, 471);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Настройки";
@@ -64,10 +66,11 @@
             // trackBar3
             // 
             this.trackBar3.BackColor = System.Drawing.Color.Pink;
-            this.trackBar3.Location = new System.Drawing.Point(6, 250);
+            this.trackBar3.Location = new System.Drawing.Point(9, 385);
+            this.trackBar3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.trackBar3.Maximum = 120;
             this.trackBar3.Name = "trackBar3";
-            this.trackBar3.Size = new System.Drawing.Size(302, 45);
+            this.trackBar3.Size = new System.Drawing.Size(453, 69);
             this.trackBar3.TabIndex = 5;
             this.trackBar3.Value = 120;
             this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll_1);
@@ -76,20 +79,22 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(6, 223);
+            this.label3.Location = new System.Drawing.Point(9, 343);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(195, 23);
+            this.label3.Size = new System.Drawing.Size(292, 33);
             this.label3.TabIndex = 4;
             this.label3.Text = "Приблизить и отдалить";
             // 
             // trackBar2
             // 
             this.trackBar2.BackColor = System.Drawing.Color.Pink;
-            this.trackBar2.Location = new System.Drawing.Point(6, 152);
+            this.trackBar2.Location = new System.Drawing.Point(9, 234);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.trackBar2.Maximum = 90;
             this.trackBar2.Minimum = -90;
             this.trackBar2.Name = "trackBar2";
-            this.trackBar2.Size = new System.Drawing.Size(299, 45);
+            this.trackBar2.Size = new System.Drawing.Size(448, 69);
             this.trackBar2.TabIndex = 3;
             this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll_1);
             // 
@@ -97,9 +102,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(6, 123);
+            this.label2.Location = new System.Drawing.Point(9, 189);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(111, 23);
+            this.label2.Size = new System.Drawing.Size(167, 33);
             this.label2.TabIndex = 2;
             this.label2.Text = "Выше и ниже";
             // 
@@ -107,10 +113,11 @@
             // 
             this.trackBar1.BackColor = System.Drawing.Color.Pink;
             this.trackBar1.Cursor = System.Windows.Forms.Cursors.VSplit;
-            this.trackBar1.Location = new System.Drawing.Point(6, 42);
+            this.trackBar1.Location = new System.Drawing.Point(9, 65);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.trackBar1.Maximum = 360;
             this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(302, 45);
+            this.trackBar1.Size = new System.Drawing.Size(453, 69);
             this.trackBar1.SmallChange = 5;
             this.trackBar1.TabIndex = 1;
             this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll_1);
@@ -119,9 +126,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Location = new System.Drawing.Point(4, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 23);
+            this.label1.Size = new System.Drawing.Size(193, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Влево и вправо";
             // 
@@ -133,7 +141,8 @@
             this.gmap.GrayScaleMode = false;
             this.gmap.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
             this.gmap.LevelsKeepInMemmory = 5;
-            this.gmap.Location = new System.Drawing.Point(12, 12);
+            this.gmap.Location = new System.Drawing.Point(18, 18);
+            this.gmap.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.gmap.MarkersEnabled = true;
             this.gmap.MaxZoom = 2;
             this.gmap.MinZoom = 2;
@@ -147,32 +156,34 @@
             this.gmap.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
             this.gmap.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
             this.gmap.ShowTileGridLines = false;
-            this.gmap.Size = new System.Drawing.Size(753, 306);
+            this.gmap.Size = new System.Drawing.Size(1130, 471);
             this.gmap.TabIndex = 5;
             this.gmap.Zoom = 0D;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(26, 329);
+            this.pictureBox1.Location = new System.Drawing.Point(39, 506);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1059, 408);
+            this.pictureBox1.Size = new System.Drawing.Size(1588, 628);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 34;
             this.pictureBox1.TabStop = false;
             // 
             // Panorama
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.PeachPuff;
-            this.ClientSize = new System.Drawing.Size(1097, 749);
+            this.ClientSize = new System.Drawing.Size(1636, 1126);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.gmap);
             this.Controls.Add(this.groupBox1);
-            this.MaximumSize = new System.Drawing.Size(1113, 788);
-            this.MinimumSize = new System.Drawing.Size(1113, 726);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximumSize = new System.Drawing.Size(1658, 1182);
+            this.MinimumSize = new System.Drawing.Size(1658, 1087);
             this.Name = "Panorama";
-            this.Text = "Panorama";
+            this.Text = "ооооооо";
             this.Load += new System.EventHandler(this.Panorama_Load);
             this.Resize += new System.EventHandler(this.Panorama_Resize);
             this.groupBox1.ResumeLayout(false);
