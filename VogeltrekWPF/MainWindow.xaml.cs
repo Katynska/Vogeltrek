00d1efb2fa847a52b6a958fb7aeadc6f95ebf0bb
Grigory Povolyaev IBM-PC <povolyaev@yahoo.com>
2024-04-21 23:25:52 +0300

    Добавил окно Анкеты и изменил версию расширения Extended WPF Toolkit на 3.6.0 (бесплатную)

--- VogeltrekWPF/MainWindow.xaml.cs
+++ VogeltrekWPF/MainWindow.xaml.cs
@@ -24,21 +24,21 @@ namespace VogeltrekWPF
         {
             InitializeComponent();
         }
-        private void mapView_Loaded(object sender, RoutedEventArgs e)
+        private void mapSurvey_Loaded(object sender, RoutedEventArgs e)
         {
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
             // choose your provider here
-            mapView.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
-            mapView.MinZoom = 2;
-            mapView.MaxZoom = 17;
+            mapSurvey.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+            mapSurvey.MinZoom = 2;
+            mapSurvey.MaxZoom = 17;
             // whole world zoom
-            mapView.Zoom = 2;
+            mapSurvey.Zoom = 2;
             // lets the map use the mousewheel to zoom
-            mapView.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            mapSurvey.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
             // lets the user drag the map
-            mapView.CanDragMap = true;
+            mapSurvey.CanDragMap = true;
             // lets the user drag the map with the left mouse button
-            mapView.DragButton = MouseButton.Left;
+            mapSurvey.DragButton = MouseButton.Left;
         }
     }
 }
