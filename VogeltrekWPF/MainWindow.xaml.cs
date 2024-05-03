c018c2c404ea0d68407bb1d9fded52e5c2884bef
Grigory_VScode <povolyaev@yahoo.com>
2024-05-03 13:53:44 +0300

    Загрузку карты Gmap вывел в отдельный файл

--- VogeltrekWPF/MainWindow.xaml.cs
+++ VogeltrekWPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VogeltrekWPF.Scripts;
 
 namespace VogeltrekWPF
 {
@@ -42,19 +43,7 @@ namespace VogeltrekWPF
 
         private void mapSurvey_Loaded(object sender, RoutedEventArgs e)
         {
-            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerAndCache;
-            // choose your provider here
-            mapSurvey.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
-            mapSurvey.MinZoom = 2;
-            mapSurvey.MaxZoom = 17;
-            // whole world zoom
-            mapSurvey.Zoom = 2;
-            // lets the map use the mousewheel to zoom
-            mapSurvey.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
-            // lets the user drag the map
-            mapSurvey.CanDragMap = true;
-            // lets the user drag the map with the left mouse button
-            mapSurvey.DragButton = MouseButton.Left;
+            GmapSheet.ConfigureMap(mapSurvey);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
---
+++ VogeltrekWPF/Scripts/GmapSheet.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
+
+
+namespace VogeltrekWPF.Scripts
+{
+    internal class GmapSheet
+    {
+        public static void ConfigureMap(GMapControl map)
+        {
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            // choose your provider here
+            map.MapProvider = GMap.NET.MapProviders.OpenStreetMapProvider.Instance;
+            map.MinZoom = 2;
+            map.MaxZoom = 17;
+            // whole world zoom
+            map.Zoom = 2;
+            // lets the map use the mousewheel to zoom
+            map.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            // lets the user drag the map
+            map.CanDragMap = true;
+            // lets the user drag the map with the left mouse button
+            map.DragButton = MouseButton.Left;
+        }
+    }
+}
